47b09844ae48338d5a22546de0fd9a4dea9d0dd1
VardhanNS <vardhan@edureka.co>
2017-08-01 21:13:40 +0530

    foobar

--- src/app/bold-text.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BoldTextDirective } from './bold-text.directive';
-
-describe('BoldTextDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BoldTextDirective();
-    expect(directive).toBeTruthy();
-  });
-});
